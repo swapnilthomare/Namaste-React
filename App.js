8d23e07bee6d3404353f360686dc95b7714d59b8
Swapnil Thomare <swapnil2001thomare@gmail.com>
2023-05-06 13:53:28 +0530

    learn abot  Parcel

--- App.js
+++ App.js
@@ -1,19 +1,10 @@
-/**
- * 
- * <div id="parent">
- *       <div id="child">
- *           <h1>I'am H! tag</h1>
- *           <h1>I'am H! tag</h1>
- *           
- *       <div>
- * 
- * ReactElement(object) => HTML(browser understand)
- */
+import React from "react";
+import ReactDOM from "react-dom/client"
 
 const parent = React.createElement("div",{id:"parent"},
     [
         React.createElement("div",{id:"Child1"},
-            [React.createElement("h1",{id:"heading"},"hello React tree"),
+            [React.createElement("h1",{id:"heading"},"This is namaste react"),
             React.createElement("h1",{id:"heading2"},"hello sibling 2")]
             
             ),
@@ -31,13 +22,20 @@ const parent = React.createElement("div",{id:"parent"},
 
 
 
-const heading = React.createElement("h1",
-{id:"heading"},
-"Hello world from react!"
-); //object
+// const heading = React.createElement("h1",
+// {id:"heading"},
+// "Hello world from react!"
+// ); //object
+
+//const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(parent);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 //root.render(heading); // convert heading object into html tag and pass to browser
-root.render(parent); 
-console.log(parent);
+root.render(parent);
+
+
+
+
+
